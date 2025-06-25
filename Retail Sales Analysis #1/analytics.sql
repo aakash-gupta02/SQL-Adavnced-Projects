62f5bdba14b29455ccf23ff2402dea0677aaaf11
Aakash <editsky597@gmail.com>
2025-06-25 20:27:46 +0530

    analytics

---
+++ Retail Sales Analysis #1/analytics.sql
@@ -0,0 +1,19 @@
+# Q1. Retrieve all columns for sales made on '2022-11-05'
+
+# Q2. Retrieve all transactions where the category is 'Clothing' and quantity sold is more than 4 in November 2022
+
+# Q3. Calculate the total sales (total_sale) for each category
+
+# Q4. Find the average age of customers who purchased items from the 'Beauty' category
+
+# Q5. Find all transactions where the total_sale is greater than 1000
+
+# Q6. Find the total number of transactions made by each gender in each category
+
+# Q7. Calculate the average sale for each month and find the best-selling month in each year
+
+# Q8. Find the top 5 customers based on the highest total sales
+
+# Q9. Find the number of unique customers who purchased items from each category
+
+# Q10. Create each shift (Morning, Afternoon, Evening) and find number of orders in each
